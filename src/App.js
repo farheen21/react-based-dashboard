8940d1777ae04f248237afcedf649a1f5b202fc2
Farheen khan <farheenkhancc@gmail.com>
2022-10-08 21:25:09 +0530

    Folder Structure Implemented

--- src/App.js
+++ src/App.js
@@ -2,8 +2,9 @@ import React, { useEffect } from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { FiSettings } from 'react-icons/fi';
 import { TooltipComponent } from '@syncfusion/ej2-react-popups';
-
-
+import {Navbar , Footer , Sidebar , ThemeSetting } from './components'
+import { Ecommerce, Orders, Calendar, Employees, Stacked, Pyramid, Customers, Kanban, Line, Area, Bar, Pie, Financial, ColorPicker, ColorMapping, Editor } from './pages';
+import './App.css';
 import './App.css';
 function App() {
   const activeMenu = true;
@@ -17,41 +18,41 @@ function App() {
             </button>
           </TooltipComponent>
         </div>
-        {activeMenu ? (<div className='w-72 fixed sidebar dark:bg-secondary-dark-bg bg-white'> Sidebar </div>) : <div className='w-0 bg-secondary-dark-bg ' > Sidebar</div>}
+        {activeMenu ? (<div className='w-72 fixed sidebar dark:bg-secondary-dark-bg bg-white'> <Sidebar /> </div>) : <div className='w-0 bg-secondary-dark-bg ' > Sidebar</div>}
         <div className={
           `dark:bg-main-bg bg-main-bg min-h-screen  w-full ${activeMenu ? 'mid:ml-72' : 'flex-2'}`
            }>
 
         </div>
         <div className='md:static fixed bg-main-bg  dark:bg-main-dark-bg navbar w-full'>
-           Navbar
+          <Navbar />
         </div>
         <div>
           <Routes>
-            {/* Dashboard */}
-            <Route path='/' element="Ecommerce"/>
-            <Route path='/ecommerce' element="Ecommerce"/>
+            {/* dashboard  */}
+            <Route path="/" element={(<Ecommerce />)} />
+                <Route path="/ecommerce" element={(<Ecommerce />)} />
 
-            {/* Pages  */}
-            <Route path='/orders' element="Orders"/>
-            <Route path='/employees' element="Employees"/>
-            <Route path='/customers' element="Customers"/>
+                {/* pages  */}
+                <Route path="/orders" element={<Orders />} />
+                <Route path="/employees" element={<Employees />} />
+                <Route path="/customers" element={<Customers />} />
 
-            {/* Apps */}
-            <Route path='/kanban' element="Kanban"/>
-            <Route path='/editors' element="Editors"/>
-            <Route path='/calender' element="Calenders"/>
-            <Route path='/color-picker' element="ColorPicker"/>
-            
-            {/* Charts */}
-            <Route path='/line' element="Line"/>
-            <Route path='/area' element="Area"/>
-            <Route path='/bar' element="Bar"/>
-            <Route path='/pie' element="Pie"/>
-            <Route path='/financial' element="Financial"/>
-            <Route path='/color-mapping' element="ColorMapping"/>
-            <Route path='/pyramid' element="Pyramid"/>
-            <Route path='/stacked' element="Stacked"/>
+                {/* apps  */}
+                <Route path="/kanban" element={<Kanban />} />
+                <Route path="/editor" element={<Editor />} />
+                <Route path="/calendar" element={<Calendar />} />
+                <Route path="/color-picker" element={<ColorPicker />} />
+
+                {/* charts  */}
+                <Route path="/line" element={<Line />} />
+                <Route path="/area" element={<Area />} />
+                <Route path="/bar" element={<Bar />} />
+                <Route path="/pie" element={<Pie />} />
+                <Route path="/financial" element={<Financial />} />
+                <Route path="/color-mapping" element={<ColorMapping />} />
+                <Route path="/pyramid" element={<Pyramid />} />
+                <Route path="/stacked" element={<Stacked />} />
           
           </Routes>
         </div>
---
+++ src/components/Charts/SparkLine.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Sparkline() {
+  return (
+    <div>Sparkline</div>
+  )
+}
+
+export default Sparkline
